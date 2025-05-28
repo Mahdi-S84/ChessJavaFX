06a5d2c705586e7ce2a65bb52248b3452813c6a9
Azarioun <mohammadparsaazarioun@gmail.com>
2025-05-28 18:13:32 +0330

    Add new features

---
+++ src/Board.java
@@ -0,0 +1,9 @@
+public class  Board {
+    static int  rows=8;
+    static int columns=8;
+    String[][] board = new String[rows][columns];
+    // board[0][0] = "WR";
+    // board[0][7] = "WR";
+    // board[0][1] = "WN";
+    
+}
