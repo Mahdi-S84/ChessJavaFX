5b2a892155ae143ee0b6bd6a039d0bfe31005189
Mahdi-S84 <mahdisedghi848484@gmail.com>
2025-05-29 13:26:40 +0330

    fix knight move and king check function

--- src/King.java
+++ src/King.java
@@ -4,6 +4,23 @@ public class King extends Piece {
         super(color,i,j,"king");
     }
 
+    public boolean isCheck(Spaceoccupier[][] board) {
+        for (int x = 0; x < 8; x++) {
+            for (int y = 0; y < 8; y++) {
+                Spaceoccupier piece = board[x][y];
+
+                if (piece != null && !piece.getName().equals("empty") && piece.color != this.color) {
+                    if (piece.isValidMove(this.i, this.j)) {
+                        // for see can our piece kill our king?
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+
     public boolean isValidMove(int i, int j){
         return true;
     }
--- src/Knight.java
+++ src/Knight.java
@@ -4,12 +4,25 @@ public class Knight extends Piece {
         super(color,i,j,"knight");
     }
 
-    public boolean isValidMove(int i, int j){
-        return true;
+    public boolean canGo(int fi, int fj, Spaceoccupier[][] board) {
+        Spaceoccupier piece = board[fi][fj];
+        if (piece.color != this.color) {
+            return true;
+        }
+        return false;
+    }
+    public boolean isValidMove(int fi, int fj, Spaceoccupier[][] board){
+        boolean state = false;
+        if ((((fi == i - 2) || (fi == i + 2)) && ((fj == j - 1) || (fj == j + 1))) ||
+            (((fi == i - 1) || (fi == i + 1)) && ((fj == j - 2) || (fj == j + 2)))) {
+
+            state = canGo(fi, fj, board);
+        }
+        return state;
     }
 
     public void move(int fi,int fj, Spaceoccupier[][] board){
-        if(isValidMove(fi,fj)){
+        if(isValidMove(fi, fj, board)){
             board[fi][fj]=this;
             board[this.i][this.j] = new Empty();
             this.i = fi;
